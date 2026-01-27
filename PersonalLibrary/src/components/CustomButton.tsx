dbfa36ed7166b57a78678409ebcc43ed8bbbc568
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-01-26 21:00:10 -0600

    -commit inicial creando proyecto react native con expo -agregando componente reutilizable CustomButton

---
+++ PersonalLibrary/src/components/CustomButton.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function CustomButton (){
+
+    return(
+        <View>
+            <Text>Soy un Boton</Text>
+        </View>
+    );
+}
